6fda9add96ea458427a9c1c2dd09b59781df6eca
Sohamambre07 <82446205+Sohamambre07@users.noreply.github.com>
2021-06-03 21:21:30 +0530

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,34 @@
+class Slingshot{
+
+constructor(a,b){
+
+   var options={
+    bodyA:a,
+    pointB:b,
+    length:10,
+stiffness:0.03
+   }
+
+     this.sling=Constraint.create(options)
+     World.add(world,this.sling)
+
+   this.pointB
+   }
+ display(){
+
+    if(this.sling.bodyA){
+strokeWeight(3)
+
+line (this.sling.bodyA.position.x, this.sling.bodyA.position.y,
+      this.sling.pointB.x,this.sling.pointB.y)
+ }
+}
+
+fly(){
+this.sling.bodyA=null
+}
+attach(body){
+this.sling.bodyA=body
+}
+
+}
